115e72dde49569f6c36d564e3737ae2e582ae1cd
Rahul <87407780+wreck2053@users.noreply.github.com>
2024-05-19 23:22:40 +0530

    Added fan

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,24 @@
     "1b0bc916-d160-419e-9beb-fd2935a5ddbc-13efd354-02a6-4a0b-8e5c-" \
     "1765edb0b078"
 
-#define LIGHT_RELAY_PIN 5
+int previousLightState = LOW;
+#define LIGHT_RELAY_PIN 18
+#define LIGHT_SWITCH_PIN 12
 #define SWITCH_ID_LIGHT "664268b26443b9bfe2b8d715"
+
+int previousFanState = LOW;
+#define FAN_RELAY_PIN 5
+#define FAN_SWITCH_PIN 13
+#define SWITCH_ID_FAN "6644c9cf6443b9bfe2b9dfb5"
 
 AsyncWebServer server(80);
 
 void setupDevices() {
     pinMode(BUILTIN_LED, OUTPUT);
     pinMode(LIGHT_RELAY_PIN, OUTPUT);
+    pinMode(LIGHT_SWITCH_PIN, INPUT_PULLUP);
+    pinMode(FAN_RELAY_PIN, OUTPUT);
+    pinMode(FAN_SWITCH_PIN, INPUT_PULLUP);
 }
 
 void setupWiFi() {
@@ -42,7 +52,13 @@ void setupWiFi() {
 
 bool onPowerStateLight(const String& deviceId, bool& state) {
     Serial.printf("Light turned %s\n", state ? "on" : "off");
-    digitalWrite(LIGHT_RELAY_PIN, state ? HIGH : LOW);
+    digitalWrite(LIGHT_RELAY_PIN, state ? LOW : HIGH);
+    return true;
+}
+
+bool onPowerStateFan(const String& deviceId, bool& state) {
+    Serial.printf("Fan turned %s\n", state ? "on" : "off");
+    digitalWrite(FAN_RELAY_PIN, state ? LOW : HIGH);
     return true;
 }
 
@@ -50,6 +66,9 @@ void setupSinricPro() {
     // add devices and callbacks to SinricPro
     SinricProSwitch& lightSwitch = SinricPro[SWITCH_ID_LIGHT];
     lightSwitch.onPowerState(onPowerStateLight);
+
+    SinricProSwitch& fanSwitch = SinricPro[SWITCH_ID_FAN];
+    fanSwitch.onPowerState(onPowerStateFan);
 
     // setup SinricPro
     SinricPro.onConnected([]() {
@@ -68,18 +87,36 @@ void setupSinricPro() {
 void setupServer() {
     // Route for root / web page
     server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
-        request->send(
-            200, "text/html",
-            "<h1>ESP32 Server</h1><button onclick=\"toggleSwitch()\">Toggle "
-            "Switch</button><script>function toggleSwitch() {var xhr = new "
-            "XMLHttpRequest();xhr.open('GET', '/toggle', "
-            "true);xhr.send();}</script>");
+        request->send(200, "text/html",
+                      "<h1>NodeMCU-32S Server</h1>"
+                      "<button onclick=\"toggleLight()\">Toggle Light</button>"
+                      "<br>"
+                      "<br>"
+                      "<button onclick=\"toggleFan()\">Toggle Fan</button>"
+                      "<script>"
+                      "function toggleLight() {"
+                      "    var xhr = new XMLHttpRequest();"
+                      "    xhr.open('GET', '/toggle-light', true);"
+                      "    xhr.send();"
+                      "}"
+                      "function toggleFan() {"
+                      "    var xhr = new XMLHttpRequest();"
+                      "    xhr.open('GET', '/toggle-fan', true);"
+                      "    xhr.send();"
+                      "}"
+                      "</script>");
     });
 
-    // Route to handle LED toggle
-    server.on("/toggle", HTTP_GET, [](AsyncWebServerRequest* request) {
+    // Route to handle Light toggle
+    server.on("/toggle-light", HTTP_GET, [](AsyncWebServerRequest* request) {
         digitalWrite(LIGHT_RELAY_PIN, !digitalRead(LIGHT_RELAY_PIN));
-        request->send(200, "text/plain", "LED toggled");
+        request->send(200, "text/plain", "Light toggled");
+    });
+
+    // Route to handle Fan toggle
+    server.on("/toggle-fan", HTTP_GET, [](AsyncWebServerRequest* request) {
+        digitalWrite(FAN_RELAY_PIN, !digitalRead(FAN_RELAY_PIN));
+        request->send(200, "text/plain", "Fan toggled");
     });
 
     server.begin();
@@ -99,4 +136,18 @@ void setup() {
     Serial.println("\nSetup end\n");
 }
 
-void loop() { SinricPro.handle(); }
+void loop() {
+    SinricPro.handle();
+
+    int currentLightState = digitalRead(LIGHT_SWITCH_PIN);  // light
+    if (currentLightState != previousLightState) {
+        previousLightState = currentLightState;
+        digitalWrite(LIGHT_RELAY_PIN, currentLightState);
+    }
+
+    int currentFanState = digitalRead(FAN_SWITCH_PIN);  // fan
+    if (currentFanState != previousFanState) {
+        previousFanState = currentFanState;
+        digitalWrite(FAN_RELAY_PIN, currentFanState);
+    }
+}
